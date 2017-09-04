3b72f70417ca268e20a568f62943f79d2f937a19
Mitch Hankins <mitchhankins92@gmail.com>
2017-09-03 18:56:02 -0600

    Implemented text below input displaying date and rate

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,12 +3,15 @@ import { View, StatusBar } from 'react-native';
 import { Container } from '../components/Container';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
-import { ClearButton } from '../components/Button'
+import { ClearButton } from '../components/Button';
+import { LastConverted } from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
 const TEMP_QUOTE_PRICE = '79.75';
+const TEMP_CONVERSION_RATE = 0.7974;
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends Component {
   handlePressBaseCurreny = () => {
@@ -44,6 +47,12 @@ class Home extends Component {
           onPress={this.handlePressQuoteCurreny}
           editable={false}
           value={TEMP_QUOTE_PRICE}
+          />
+          <LastConverted
+            base={TEMP_BASE_CURRENCY}
+            quote={TEMP_QUOTE_CURRENCY}
+            date={TEMP_CONVERSION_DATE}
+            conversionRate={TEMP_CONVERSION_RATE}
           />
           <ClearButton
             text='Reverse Currencies'
