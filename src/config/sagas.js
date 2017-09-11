2d0058ea0a2971ce8713813a6fae9414c62f3f8c
Mitch Hankins <mitchhankins92@gmail.com>
2017-09-10 20:24:23 -0600

    Implemented fixer.io

--- src/config/sagas.js
+++ src/config/sagas.js
@@ -1,10 +1,26 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, select, call, put } from 'redux-saga/effects';
 
-import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies';
+import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION, CONVERSION_RESULT, CONVERSION_ERROR } from '../actions/currencies';
+
+const getLatestRate = currency => fetch(`http://api.fixer.io/latest?base=${currency}`);
 
 const fetchLatestConversionRates = function* (action) {
-  console.log('update', action);
-  yield;
+  try {
+    let currency = action.currency;
+    if (currency === undefined) {
+      currency = yield select(state => state.currencies.baseCurrency);
+    }
+    const response = yield call(getLatestRate, currency);
+    const result = yield response.json();
+
+    if (result.error) {
+      yield put({ type: CONVERSION_ERROR, error: result.error });
+    } else {
+      yield put({ type: CONVERSION_RESULT, result });
+    }
+  } catch (e) {
+    yield put({ type: CONVERSION_ERROR, error: e.message });
+  }
 };
 
 const rootSaga = function* () {
--- src/reducers/currencies.js
+++ src/reducers/currencies.js
@@ -1,4 +1,12 @@
-import { CHANGE_CURRENCY_AMOUNT, SWAP_CURRENCY, CHANGE_BASE_CURRENCY, CHANGE_QUOTE_CURRENCY } from '../actions/currencies';
+import {
+  CHANGE_CURRENCY_AMOUNT,
+  SWAP_CURRENCY,
+  CHANGE_BASE_CURRENCY,
+  CHANGE_QUOTE_CURRENCY,
+  GET_INITIAL_CONVERSION,
+  CONVERSION_RESULT,
+  CONVERSION_ERROR,
+} from '../actions/currencies';
 
 const initialState = {
   baseCurrency: 'USD',
@@ -48,6 +56,27 @@ const reducer = (state = initialState, action) => {
         ...state,
         quoteCurrency: action.currency,
         conversions: setConversions(state, action),
+      };
+    case GET_INITIAL_CONVERSION:
+      return {
+        ...state,
+        conversions: setConversions(state, { currency: state.baseCurrency })
+      };
+    case CONVERSION_RESULT:
+     return {
+       ...state,
+       baseCurrency: action.result.base,
+       conversions: {
+         ...state.conversions,
+       [action.result.base]: {
+         ...action.result,
+       },
+      },
+     };
+    case CONVERSION_ERROR:
+      return {
+        ...state,
+        error: action.error,
       };
     default:
       return state;
